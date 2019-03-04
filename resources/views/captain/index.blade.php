8aacb3d62996f7a3f98ab5ea9d86bb4f6263ff69
TPolan <t.poly@seznam.cz>
2019-03-04 11:19:37 +0100

    roster changes

--- resources/views/captain/index.blade.php
+++ resources/views/captain/index.blade.php
@@ -3,29 +3,21 @@
 @section('content')
 
 <h1>The captain roster</h1>
+<h2>Choose your captain: </h2>
 
-@foreach ($captains as $captain)
-    
+
 <section class="info">
-
+    @foreach ($captains as $captain)
+    <a href="{{action('CaptainController@show',$captain->slug)}}">
     <img class="portrait" src="{{ asset('img/'.$captain->slug.'.jpg') }}" alt="">
-
+    </a>
     <div class="data">
         <h1>{{ $captain->name }}</h1>
-        <a href="{{action('CaptainController@show',$captain->slug)}}" class="report"> <button>Assign a captain</button></a>
-        <div class="story">
-            {!! nl2br($captain->story) !!}
-        </div>
-    </div>
 
-    <div class="service">
-        <h2>In service</h2>
-        From {{ date('g:i A', strtotime($captain->serves_from)) }} to {{ date('g:i A', strtotime($captain->serves_until)) }}.
-
-        <div class="status unavailable">Current status: <span>Not in service</span></div>
     </div>
+    @endforeach
+
 
 </section>
-@endforeach
     
 @endsection
